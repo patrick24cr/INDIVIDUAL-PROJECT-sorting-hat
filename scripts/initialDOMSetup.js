721ef0c7f98bba353f0e2a48d59f4f5a57a4d1a3
Patrick Burns <burns212@gmail.com>
2022-05-06 11:01:38 -0500

    I think it's done

---
+++ scripts/initialDOMSetup.js
@@ -0,0 +1,43 @@
+import returnHouseDOMString from "./returnHouseDOMString.js";
+import { renderToDom } from "./utilities.js";
+
+function initialDOMSetup(){
+  const initialHTML = `
+<div class="background">
+  <div class="title" id="title">
+    <img src="images/hogwarts.png" alt="hogwarts coat of arms" id="hogwarts-btn">
+    <H1>Sorting Ceremony</H1>
+    <img src="images/voldemort.png" alt="symbol of voldemort" id="voldemort-btn">
+  </div>
+  <div class="hat-background">
+    <div class="hat-container">
+      <div class="hat-img">
+        <img src="images/hat.png" alt="the sorting hat">
+      </div>
+      <div class="hat-text" id="hat-text">
+        <p>Would you like to begin?</p>
+      </div>
+    </div>
+  </div>
+  <div id="input-container">
+    <button type="button" class="btn btn-secondary btn-lg" id="begin-btn">Let it begin</button>
+  </div>
+  <div class="house-container" id="house-container">
+  <div class="gryffindor" id="gryffindor">
+  ${returnHouseDOMString("gryffindor")}
+</div>
+<div class="hufflepuff" id="hufflepuff">
+  ${returnHouseDOMString("hufflepuff")}
+</div>
+<div class="ravenclaw" id="ravenclaw">
+  ${returnHouseDOMString("ravenclaw")}
+</div>
+<div class="slytherin" id="slytherin">
+  ${returnHouseDOMString("slytherin")}
+</div>
+  </div>
+</div>`
+  renderToDom("#bodyDiv", initialHTML)
+}
+
+export default initialDOMSetup;
---
+++ scripts/utilities.js
@@ -0,0 +1,33 @@
+function renderToDom(target, string) {
+  document.querySelector(target).innerHTML = string;
+}
+
+function fadeOutEffect(thingToFadeOut, divToRedefine, stringToInsert, thingToFadeIn) {
+  var fadeTarget = document.querySelector(thingToFadeOut);
+  var opacityCounter = 1;
+  var fadeTimer = setInterval(function () {
+      if (opacityCounter > 0) {
+          fadeTarget.style.opacity = opacityCounter;
+          opacityCounter -= 0.1;
+      } else {
+          clearInterval(fadeTimer);
+          renderToDom(divToRedefine, stringToInsert);
+          fadeInEffect(thingToFadeIn);
+      }
+  }, 50);
+}
+
+function fadeInEffect(thingToFadeIn) {
+  var opacityCounter = 0;
+  var fadeTarget = document.querySelector(thingToFadeIn);
+  var fadeTimer = setInterval(function () {
+      if (opacityCounter < 1) {
+          fadeTarget.style.opacity = opacityCounter;
+          opacityCounter += 0.1;
+      } else {
+          clearInterval(fadeTimer);
+      }
+  }, 50);
+}
+
+export {renderToDom, fadeOutEffect, fadeInEffect};
